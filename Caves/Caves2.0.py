cfe06b681db329bdf5a619e78684c680d3a07aea
AlexandreMakovetsky <alexander.a.makovetsky@gmail.com>
2014-02-09 18:57:09 -0600

    v2.0+

--- Caves/Caves2.0.py
+++ Caves/Caves2.0.py
@@ -15,16 +15,20 @@ _Room2 = [200, 'dark', 'bats', 'spiders']
 _Room3 = [500, 'limestone', 'chalk', 'dolomite', 'marble', 'salt', 'gypsum']
 _Room4 = [750, 'fish', 'amphibians', 'reptiles', 'worms', 'Crustacea']
 _Room5 = [1000, 'gold']
-_Cave1 = ['Room1', 'Room2', 'Room3', 'Room4']
+_Cave1 = ['Room 1', 'Room 2', 'Room 3', 'Room 4']
 _roomItems = []
 _pointsTotal = []
 
 ###Characters
 _character = ['c1', 'c2', 'c3']
 _dwarfCharacter = 'Dwarf'
-_dwarfItems = ['rope', 'flash light', 'hammer']
+_dwarfItems = ['rope', 'flash light', 'hammer', 'carabiner', 'axe', 'climbing harness', 'quickdraw']
+_ItemsPoints = [100, 200, 300, 400, 500, 600, 700 ]
 _dwarfCurrentItems = []
 _dwarfActivate = []
+_dwarfCurrentRoom = []
+_characterCurrentRoom = []
+
 
 def activateDwarf():
     if sum(_pointsTotal) > 500:
@@ -84,22 +88,30 @@ _observeItem = []
 def explore():
     if _exploreRooms == 'Yes':
         _roomItems.append(_Room1[1:] + _Room2[1:] + _Room3[1:] + _Room4[1:])
-        print(_roomItems)
+        print(_roomItems[0])
 
     _observeItem = input('Which Item do You Want to Observe: ')
 
     if _observeItem in _Room1:
-        print('You are in Room 1')
+        _characterCurrentRoom.append(_Cave1[0])
+        print(_characterCurrentRoom)
         _pointsTotal.append(_Room1[0])
     elif _observeItem in _Room2:
-        print('You are in Room 2')
+        _characterCurrentRoom.append(_Cave1[1])
+        print(_characterCurrentRoom)
         _pointsTotal.append(_Room2[0])
     elif _observeItem in _Room3:
-        print('You are in Room 3')
+        _characterCurrentRoom.append(_Cave1[2])
+        print(_characterCurrentRoom)
         _pointsTotal.append(_Room3[0])
     elif _observeItem in _Room4:
-        print('You are in Room 4')
+        _characterCurrentRoom.append(_Cave1[3])
+        print(_characterCurrentRoom)
         _pointsTotal.append(_Room4[0])
+
+    if len(_characterCurrentRoom) == 1:
+        _characterCurrentRoom.clear()
+
 
 
 def explorePointTotal():
@@ -133,11 +145,24 @@ explorePointTotal()
 
 activateDwarf()
 
-
-
 print('I AM HERE IN CODE')
-_dwarfRoom = random.choice(_Cave1)
-print('Dwarf is in ' + _dwarfRoom)
+
+
+def dwarfLocation():
+    _dwarfRoom = random.choice(_Cave1)
+    _dwarfCurrentRoom.append(_dwarfRoom)
+
+    if len(_dwarfCurrentRoom) != 0:
+        _dwarfCurrentRoom.clear()
+
+    print('Dwarf is in ' + _dwarfRoom)
+
+
+dwarfLocation()
+
+
+
+
 
 
 
