dbcf5b01fc22705932450c717b2112fed021d81a
AlexandreMakovetsky <alexander.a.makovetsky@gmail.com>
2014-02-15 14:31:00 -0600

    v2.0+

--- Caves/Caves2.0.py
+++ Caves/Caves2.0.py
@@ -9,25 +9,33 @@ print('by VHS')
 print('---' * 25)
 print('---' * 25)
 
-### Room and Cave Items
-_Room1 = [100, 'light', 'water', 'plants']
-_Room2 = [200, 'dark', 'bats', 'spiders']
-_Room3 = [500, 'limestone', 'chalk', 'dolomite', 'marble', 'salt', 'gypsum']
-_Room4 = [750, 'fish', 'amphibians', 'reptiles', 'worms', 'Crustacea']
-_Room5 = [1000, 'gold']
-_Cave1 = ['Room 1', 'Room 2', 'Room 3', 'Room 4']
-_roomItems = []
+#Features and points of individual caves within the cave system
+_cave1 = [100, 'light', 'water', 'plants']
+_cave2 = [200, 'dark', 'bats', 'spiders']
+_cave3 = [300, 'limestone', 'chalk', 'dolomite', 'marble', 'salt', 'gypsum']
+_cave4 = [400, 'fish', 'amphibians', 'reptiles', 'worms', 'Crustacea']
+_cave5 = [500, 'buddhist iconography']
+_cave6 = [650, 'hercules slept here']
+_cave7 = [750, 'boxwork']
+_cave8 = [850, 'grotto']
+_cave9 = [950, 'crystals']
+_cave10 = [1500, 'jungle', 'monkeys', 'flying foxes']
+_cave11 = [1000, 'gold']
+_caveSystem = ['Cave 1', 'Cave 2', 'Cave 3', 'Cave 4',
+               'Cave 5', 'Cave 6', 'Cave 7', 'Cave 8', 'Cave 9', 'Cave 10']
+_roomFeatures = []
 _pointsTotal = []
 
 ###Characters
 _character = ['c1', 'c2', 'c3']
 _dwarfCharacter = 'Dwarf'
 _dwarfItems = ['rope', 'flash light', 'hammer', 'carabiner', 'axe', 'climbing harness', 'quickdraw']
-_ItemsPoints = [100, 200, 300, 400, 500, 600, 700 ]
+_ItemsPoints = [100, 200, 300, 400, 500, 600, 700]
 _dwarfCurrentItems = []
 _dwarfActivate = []
 _dwarfCurrentRoom = []
 _characterCurrentRoom = []
+_characterRoomCompare = []
 
 
 def activateDwarf():
@@ -36,6 +44,7 @@ def activateDwarf():
 
     if sum(_dwarfActivate) == 1:
         print('My name is', _dwarfCharacter, ', I am in this cave in search of Gold.', )
+
 
 ###START GAME
 _continue = 'Continue'
@@ -64,19 +73,19 @@ else:
 _currentRoomLocation = []
 _darkOrLight = input('Do you want to enter cave in the dark or light?: ')
 
-if _darkOrLight in _Room1:
+if _darkOrLight in _cave1:
     print('You are in Room 1')
-    print('This room has', _Room1[1:])
-elif _darkOrLight in _Room2:
+    print('This room has', _cave1[1:])
+elif _darkOrLight in _cave2:
     print('You are in Room 2')
-    print('This room has', _Room2[1:])
+    print('This room has', _cave2[1:])
 else:
     print('FAILED')
 
-if _darkOrLight in _Room1:
-    _pointsTotal.append(_Room1[0])
-elif _darkOrLight in _Room2:
-    _pointsTotal.append(_Room2[0])
+if _darkOrLight in _cave1:
+    _pointsTotal.append(_cave1[0])
+elif _darkOrLight in _cave2:
+    _pointsTotal.append(_cave2[0])
 
 print('Spelunker Points Total', '|', sum(_pointsTotal), '|')
 
@@ -87,31 +96,60 @@ _observeItem = []
 
 def explore():
     if _exploreRooms == 'Yes':
-        _roomItems.append(_Room1[1:] + _Room2[1:] + _Room3[1:] + _Room4[1:])
-        print(_roomItems[0])
+        _roomFeatures.append(_cave1[1:] + _cave2[1:] + _cave3[1:] + _cave4[1:] + _cave4[1:]
+                             + _cave5[1:] + _cave6[1:] + _cave7[1:] + _cave8[1:] + _cave9[1:] + _cave10[1:])
+        print(_roomFeatures[0])
 
-    _observeItem = input('Which Item do You Want to Observe: ')
+    _observeItem = input('Which cave feature do you want to observe: ')
 
-    if _observeItem in _Room1:
-        _characterCurrentRoom.append(_Cave1[0])
+    if _observeItem in _cave1:
+        _characterCurrentRoom.append(_caveSystem[0])
         print(_characterCurrentRoom)
-        _pointsTotal.append(_Room1[0])
-    elif _observeItem in _Room2:
-        _characterCurrentRoom.append(_Cave1[1])
+        _pointsTotal.append(_cave1[0])
+    elif _observeItem in _cave2:
+        _characterCurrentRoom.append(_caveSystem[1])
         print(_characterCurrentRoom)
-        _pointsTotal.append(_Room2[0])
-    elif _observeItem in _Room3:
-        _characterCurrentRoom.append(_Cave1[2])
+        _pointsTotal.append(_cave2[0])
+    elif _observeItem in _cave3:
+        _characterCurrentRoom.append(_caveSystem[2])
         print(_characterCurrentRoom)
-        _pointsTotal.append(_Room3[0])
-    elif _observeItem in _Room4:
-        _characterCurrentRoom.append(_Cave1[3])
+        _pointsTotal.append(_cave3[0])
+    elif _observeItem in _cave4:
+        _characterCurrentRoom.append(_caveSystem[3])
         print(_characterCurrentRoom)
-        _pointsTotal.append(_Room4[0])
+        _pointsTotal.append(_cave4[0])
+    elif _observeItem in _cave5:
+        _characterCurrentRoom.append(_caveSystem[4])
+        print(_characterCurrentRoom)
+        _pointsTotal.append(_cave5[0])
+    elif _observeItem in _cave6:
+        _characterCurrentRoom.append(_caveSystem[5])
+        print(_characterCurrentRoom)
+        _pointsTotal.append(_cave6[0])
+    elif _observeItem in _cave7:
+        _characterCurrentRoom.append(_caveSystem[6])
+        print(_characterCurrentRoom)
+        _pointsTotal.append(_cave7[0])
+    elif _observeItem in _cave8:
+        _characterCurrentRoom.append(_caveSystem[7])
+        print(_characterCurrentRoom)
+        _pointsTotal.append(_cave8[0])
+    elif _observeItem in _cave9:
+        _characterCurrentRoom.append(_caveSystem[8])
+        print(_characterCurrentRoom)
+        _pointsTotal.append(_cave9[0])
+    elif _observeItem in _cave10:
+        _characterCurrentRoom.append(_caveSystem[9])
+        print(_characterCurrentRoom)
+        _pointsTotal.append(_cave10[0])
+
+    _characterRoomCompare.append(_characterCurrentRoom[0])
+
+    if len(_characterRoomCompare) 
+
 
     if len(_characterCurrentRoom) == 1:
         _characterCurrentRoom.clear()
-
 
 
 def explorePointTotal():
@@ -121,12 +159,26 @@ def explorePointTotal():
         print('System Failure', '|', _pointsTotal, '|')
 
 
+def dwarfLocation():
+    if sum(_dwarfActivate) == 1:
+        _dwarfCurrentRoom.append(random.choice(_caveSystem))
+
+
+#if _dwarfCurrentRoom not in
+
+
+
+
+
+
+
 explore()
 
 explorePointTotal()
 
 activateDwarf()
-
+dwarfLocation()
+print('///////////////', _dwarfCurrentRoom)
 ###GoThroughRooms
 _exploreRooms = input('Are you ready to explore more rooms? Yes or No: ')
 
@@ -135,7 +187,8 @@ explore()
 explorePointTotal()
 
 activateDwarf()
-
+dwarfLocation()
+print('///////////////', _dwarfCurrentRoom)
 ###GoThroughRooms
 exploreRooms = input('Are you ready to explore more rooms? Yes or No: ')
 
@@ -144,22 +197,10 @@ explore()
 explorePointTotal()
 
 activateDwarf()
-
-print('I AM HERE IN CODE')
-
-
-def dwarfLocation():
-    _dwarfRoom = random.choice(_Cave1)
-    _dwarfCurrentRoom.append(_dwarfRoom)
-
-    if len(_dwarfCurrentRoom) != 0:
-        _dwarfCurrentRoom.clear()
-
-    print('Dwarf is in ' + _dwarfRoom)
-
-
 dwarfLocation()
-
+print('///////////////', _dwarfCurrentRoom)
+print('I AM HERE IN CODE')
+print(_characterRoomCompare)
 
 
 
